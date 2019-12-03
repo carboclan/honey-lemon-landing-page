8acf44e148379607b84dc13e9238fb23e3e30c07
Tal <talrasha007@gmail.com>
2019-12-03 23:55:44 +0800

    logo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="banner">
+      <div class="logo">
+        <div>Honey</div>
+        <div>Lemon</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'app'
 }
 </script>
 
+<style scoped lang="scss">
+.banner {
+  height: 70px;
+  background-color: #1c1c1c;
+  box-shadow: 5px 5px 5px #888888;
+
+  .logo {
+    background-image: url("assets/logo.png");
+    background-size: 161px;
+    width: 161px;
+    height: 124px;
+    position: absolute;
+    top: 10px;
+    left: 50px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    div {
+      margin-left: -7px;
+      font-weight: bold;
+      font-size: 20px;
+    }
+  }
+}
+</style>
+
 <style>
+html, body {
+  margin: 0;
+  padding: 0;
+}
+
+@font-face {
+  font-family: "Eras ITC";
+  src: url("assets/Eras ITC Demi.ttf");
+}
+
+@font-face {
+  font-family: "Eras ITC";
+  src: url("assets/Eras ITC Bold.ttf");
+  font-weight: bold;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Eras ITC', 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
